ac8406e89c3b991dc7a040c3c4e701fa90dda073
paulscott56 <pscott209@gmail.com>
2013-12-05 19:37:16 +0200

    neo config

--- src/main/java/za/co/paulscott/musicgraph/config/AppConfig.java
+++ src/main/java/za/co/paulscott/musicgraph/config/AppConfig.java
@@ -1,28 +1,30 @@
 package za.co.paulscott.musicgraph.config;
 
+import org.neo4j.rest.graphdb.RestGraphDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
 
 import za.co.paulscott.musicgraph.hello.HelloWorld;
 import za.co.paulscott.musicgraph.hello.impl.HelloWorldImpl;
 import za.co.paulscott.musicgraph.services.country.CountryService;
 
 @Configuration
-public class AppConfig {
+@EnableNeo4jRepositories("za.co.paulscott.musicgraph.repos")
+public class AppConfig extends Neo4jConfiguration {
+
+    @Bean
+    public RestGraphDatabase graphDatabaseService() {
+        return new RestGraphDatabase("http://localhost:7474/db/data/");
+        
+    }
 
 	@Bean(name = "helloBean")
 	public HelloWorld helloWorld() {
 		return new HelloWorldImpl();
 	}
 
-	/**
-	 * <bean id="springExampleDao"
-	 * class="com.impetus.kundera.examples.spring.SpringExampleDao"> <property
-	 * name="entityManagerFactory" ref="entityManagerFactory" /> </bean> <bean
-	 * id="entityManagerFactory" class=
-	 * "org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean">
-	 * <property name="persistenceUnitName" value="cassandra_pu" /> </bean>
-	 */
 	@Bean
 	public CountryService countryService() {
 		return new CountryService();
