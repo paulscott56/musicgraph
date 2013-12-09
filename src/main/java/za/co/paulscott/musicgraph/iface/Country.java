9565f8bab1cffb51299762f5e103f1d9f13ea7ff
paulscott56 <paul.scott@dstvo.com>
2013-12-09 09:34:37 +0200

    country stuffs

---
+++ src/main/java/za/co/paulscott/musicgraph/iface/Country.java
@@ -0,0 +1,9 @@
+package za.co.paulscott.musicgraph.iface;
+
+import za.co.paulscott.musicgraph.entities.CountryEntity;
+
+
+public interface Country {
+
+	void saveCountry(CountryEntity country);
+}
---
+++ src/main/java/za/co/paulscott/musicgraph/impl/CountryImpl.java
@@ -0,0 +1,20 @@
+package za.co.paulscott.musicgraph.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import za.co.paulscott.musicgraph.entities.CountryEntity;
+import za.co.paulscott.musicgraph.iface.Country;
+import za.co.paulscott.musicgraph.services.CountryService;
+
+public class CountryImpl implements Country {
+
+	@Autowired
+	private CountryService countryService;
+
+	@Override
+	public void saveCountry(CountryEntity country) {
+		countryService.saveCountry();
+		
+	}
+
+}
